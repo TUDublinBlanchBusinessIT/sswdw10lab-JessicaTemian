08b83e1d4f0f10994f3f1601b75e92e7ff48ad0d
JessicaTemian <B00153952@mytudublin.ie>
2024-11-19 15:55:50 +0000

    part1 done

---
+++ processForm1.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+$_SESSION['passengerFN'] = $_POST ['firstname'];
+$_SESSION['passengerSN'] = $_POST ['surname'];
+if (isset($_POST['luggage'])) {
+    $_SESSION['luggage'] = 0;
+    header('location: luggage.html');
+
+}
+else {
+    $_SESSION['luggage'] = 0;
+    header('location: finalstep.php');
+
+
+}
+
+
+
+
+?>
